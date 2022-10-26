e5e4bb07c50129836aa78df1227f1d6049d9dc2b
Callum Gill <callum.gill.1993.cg@gmail.com>
2022-10-25 17:20:20 -0700

    First commit

---
+++ dist/primitives.js
@@ -0,0 +1,8 @@
+"use strict";
+let sales = 123456789;
+let course = 'Typescript';
+let is_published = true;
+console.log(sales);
+console.log(course);
+console.log(is_published);
+//# sourceMappingURL=primitives.js.map
---
+++ src/primitives.ts
@@ -0,0 +1,24 @@
+/** 
+ * 
+Lesson 1 : Primitive Types
+
+In Typescript we do not always need to annote our variables.
+This is because the Typescript compiler can detect variables based on their value.
+
+So instead of writing the below....
+
+let sales: number = 123_456_789;
+let course: string = 'Typescript';
+let is_published: boolean = true;
+
+We can write...
+
+**/
+
+let sales = 123_456_789;
+let course = 'Typescript';
+let is_published = true;
+
+console.log(sales);
+console.log(course);
+console.log(is_published);
